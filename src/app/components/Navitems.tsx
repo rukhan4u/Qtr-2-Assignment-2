ad5afded42e5d83fd5a90030de88101234bd6c84
rukhan4u <163444214+rukhan4u@users.noreply.github.com>
2024-11-13 18:51:07 +0500

    Add files via upload
    Qtr-2-Assignment-2

---
+++ src/app/components/Navitems.tsx
@@ -0,0 +1,23 @@
+export const Items  =  [{
+    id:1,
+    title:'Home' ,
+    Link: '#'
+
+},
+{    id:2,
+    title:'Courses' ,
+    Link: '#'
+    
+},
+
+{    id:3,
+    title:'Fee Structure ' ,
+    Link: '#'
+    
+},
+
+{    id:4,
+    title:'Contact ' ,
+    Link: '#'
+    
+}]
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Hero from "../app/components/Hero"
+
+export default function Home() {
+    return (
+        <div className="bg-blue-950 w-[1500px] h-[800px] top-[100px]">
+                  
+            <Hero />                   
+        </div>
+    );
+}
